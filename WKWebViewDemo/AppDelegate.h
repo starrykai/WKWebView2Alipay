79b6657e0476aae0a5041666e9ccea69966bb6b9
kkkkk <2103321403@qq.com>
2017-08-03 12:22:46 +0800

    Initial Commit

---
+++ WKWebViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WKWebViewDemo
+//
+//  Created by 吴恺 on 2017/8/3.
+//  Copyright © 2017年 wukai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WKWebViewDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WKWebViewDemo
+//
+//  Created by 吴恺 on 2017/8/3.
+//  Copyright © 2017年 wukai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
